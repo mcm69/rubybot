e9fc3c70f7dd7aee2618a7ce948aee88e4eca04c
Yuriy Opryshko <mcm6987@gmail.com>
2013-05-21 15:18:06 +0300

    better command detection in the factory
    -pinger will not now match a -ping command

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,3 +2,13 @@ def require_all(path)
 	project_root = File.dirname(File.absolute_path(__FILE__))
 	Dir.glob(project_root + '/' + path) {|file| require file}
 end
+
+class String
+	def is_command?(command)
+		self == command || self.start_with? command + ' '
+	end
+
+	def args
+		self.split(' ', 2)[1]
+	end
+end
--- lib/pluginfactory.rb
+++ lib/pluginfactory.rb
@@ -4,9 +4,11 @@ module Pluginfactory
 		# handlerblock parameter is the method body which will handle the message if the command passed
 		# equals to the command parameter supplied
 		# helptext and about are just strings which will be the return values of same-named methods of the defined class
+		cmd = Rubybot::COMMAND_SYMBOL + command
+
 		cls = Class.new(Object) do 
 			define_method(:handle) do |message|
-				return unless message.body[0,command.length+1] == Rubybot::COMMAND_SYMBOL + command
+				return unless message.body.is_command? cmd
 				handlerblock.call message
 			end
 
@@ -15,7 +17,7 @@ module Pluginfactory
 			define_method(:about) { aboutmsg }
 		end
 
-		cls.const_set("COMMAND", command)
+		cls.const_set("COMMAND", cmd)
 
 		cls.new
 	end
