d1c6063d92c5e0a02989055c00720a4ce42c3954
Yuriy Opryshko <mcm6987@gmail.com>
2013-05-21 13:24:02 +0300

    typo in plugin factory

--- lib/pluginfactory.rb
+++ lib/pluginfactory.rb
@@ -6,7 +6,7 @@ module Pluginfactory
 		# helptext and about are just strings which will be the return values of same-named methods of the defined class
 		cls = Class.new(Object) do 
 			define_method(:handler) do |message|
-				return unless message[0,command.length+1] == Rubybot::COMMAND_SYMBOL + command
+				return unless message.body[0,command.length+1] == Rubybot::COMMAND_SYMBOL + command
 				handlerblock.call message
 			end
 
