d21aaec4b3f11b71adbdef04a5e9aa6b11806e29
Yuriy Opryshko <mcm6987@gmail.com>
2013-05-21 15:21:02 +0300

    added displaying help for a particular command

--- lib/plugins/pluginlist.rb
+++ lib/plugins/pluginlist.rb
@@ -1,8 +1,22 @@
-pluginlist = Pluginfactory.create('pluginlist', 'returns list of currently loaded plugins', 'plugin list') do |message|
+pluginlist = Pluginfactory.create('help', 'returns list of currently loaded plugins or help for a particular command', 'help') do |message|
 	response = ""
-	Rubybot.plugins.each {|p| response += "#{p.class::COMMAND}: #{p.about}\r\n"}
+	args = message.body.args
+	if(args)
+		#display help text for a particular command
+		cmd = Rubybot::COMMAND_SYMBOL+args
+		plugin = Rubybot.plugins.find {plugin.class::COMMAND == cmd}
+		if plugin
+			response = "#{cmd}: #{plugin.helptext}"
+		else
+			response = "No such command: #{cmd}"
+		end
+	else
+		#display the command list
+		Rubybot.plugins.each {|p| response += "#{p.class::COMMAND}: #{p.about}\n"}
+		response += "\nFor help on particular command type #{Rubybot::COMMAND_SYMBOL}help command"
+	end
 
-	Rubybot.skype.send_chat_message(message.convo_id, response)
+	Rubybot.skype.send_chat_message(message.convo_id, response)	
 end
 
 Rubybot.register_plugin pluginlist
