85615bc61e9a375f06e4cead0bc281adc27bdce2
Yuriy Opryshko <mcm6987@gmail.com>
2013-11-22 13:19:13 +0300

    added simple rc plugin

---
+++ lib/plugins/rc.rb
@@ -0,0 +1,7 @@
+rc = Pluginfactory.create('rc', 'echoes a message/command into the current chat', 'remote control') do |message|
+	args = message.body.args
+	return unless Rubybot::Config.is_admin? message.author
+	Rubybot.skype.send_chat_message(message.convo_id, response)
+end
+
+Rubybot.register_plugin rc
